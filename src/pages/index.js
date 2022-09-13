db9620d71509b2f96c37de5961a0677c4785db40
seongjooy <seongjooyoon@gmail.com>
2022-09-12 22:28:23 -0400

    added flexible layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <section>
-      <Navbar />
-      <div>
-        <h2>Design</h2>
-        <h3>Develop and Deploy</h3>
-        <p>UX Designer</p>
-      </div>
-    </section>
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop and Deploy</h3>
+          <p>UX Designer</p>
+        </div>
+      </section>
+    </Layout>
   )
 }
--- src/pages/projects/test.js
+++ src/pages/projects/test.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Layout from "../../components/Layout"
 
 export default function Test() {
-  return <div>test</div>
+  return (
+    <Layout>
+      <div>
+        <h2>Portfolio</h2>
+        <h3>Projects and Websites</h3>
+      </div>
+    </Layout>
+  )
 }
