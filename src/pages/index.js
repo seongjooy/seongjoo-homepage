6d039de1972ccbcf2ea4cc6cb161d3fdc299e8e5
seongjooy <seongjooyoon@gmail.com>
2022-09-12 22:14:28 -0400

    added pages and basic navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop and Deploy</h3>
+        <p>UX Designer</p>
+      </div>
+    </section>
+  )
 }
---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function index() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <h3>Projects and Websites</h3>
+    </div>
+  )
+}
