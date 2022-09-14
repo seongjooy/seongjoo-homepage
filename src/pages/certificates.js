a7d8a354403171d6387d2ff50478641bdae0d2d5
seongjooy <seongjooyoon@gmail.com>
2022-09-14 16:55:36 -0400

    added hyperlinks that direct logos to their corresponding pages

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -13,18 +13,22 @@ export default function certificates() {
             <br />
             <em>Vector Institute for Artificial Intelligence</em>
             <p>
-              • Investigated bias in pre-trained language and image datasets due
-              to bias in train sets
-              <br />• Developed awareness of disadvantaged groups impacted by
-              biased AI models
-              <br /> • Studied techniques to mitigate bias in computer vision,
-              including data augmentation and cost function constraint terms
-              <br />• Completed capstone project on identifying and mitigating
-              bias in healthcare data. Presented findings to Vector Institute
-              and IRAP staff
+              • Investigated bias in pre-trained language and
+              <br /> image datasets due to bias in train sets
+              <br />• Developed awareness of disadvantaged groups
+              <br />
+              impacted by biased AI models
+              <br /> • Studied techniques to mitigate bias in computer <br />
+              vision, including data augmentation and cost <br />
+              function constraint terms
+              <br />• Completed capstone project on identifying and <br />
+              mitigating bias in healthcare data. Presented
+              <br /> findings to Vector Institute and IRAP staff
             </p>
           </div>
-          <img className={styles.logo} src="/vector.png"></img>
+          <a href="https://vectorinstitute.ai" target="_blank" rel="noreferrer">
+            <img className={styles.logo} src="/vector.png"></img>
+          </a>
         </div>
         <div className={styles.certificates}>
           <div className={styles.description}>
@@ -36,7 +40,9 @@ export default function certificates() {
               <br />• Achieved in Fall 2019, Winter 2020
             </p>
           </div>
-          <img className={styles.logo} src="/uoft.png"></img>
+          <a href="https:/www.ece.utoronto.ca" target="_blank" rel="noreferrer">
+            <img className={styles.logo} src="/uoft.png"></img>
+          </a>
         </div>
         <div className={styles.certificates}>
           <div className={styles.description}>
@@ -48,7 +54,13 @@ export default function certificates() {
               <br />• Scored in the top 25% of participants
             </p>
           </div>
-          <img className={styles.logo} src="/uwaterloo.png"></img>
+          <a
+            href="https://www.cemc.uwaterloo.ca/contests/euclid.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className={styles.logo} src="/uwaterloo.png"></img>
+          </a>
         </div>
         <div className={styles.certificates}>
           <div className={styles.description}>
@@ -60,7 +72,13 @@ export default function certificates() {
               <br />• Placed first
             </p>
           </div>
-          <img className={styles.logo} src="/sfa.png"></img>
+          <a
+            href="https://fencing.sports.or.kr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className={styles.logo} src="/sfa.png"></img>
+          </a>
         </div>
       </div>
     </Layout>
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -7,6 +7,23 @@ export default function experience() {
     <Layout>
       <div>
         <h1>Experience</h1>
+        <div className={styles.body}>
+          <div className={styles.experience}>
+            <div className={styles.description}>
+              <b>Intern, ML Developer (May 2021 - June 2022)</b>
+              <br />
+              <em>Spass Co., Ltd.</em>
+              <p>
+                • Regional fencing tournament for U-15 athletes
+                <br />• Placed first
+              </p>
+            </div>
+
+            <a href="https://spass.ai" target="_blank" rel="noreferrer">
+              <img className={styles.logo} src="/spass.png"></img>
+            </a>
+          </div>
+        </div>
       </div>
     </Layout>
   )
